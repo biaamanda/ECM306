0805d6d7abd5c98c4fdc6e51576fa94b4e46df34
Amanda Bialer <amandabialer@gmail.com>
2026-02-18 22:04:04 -0300

    continue L1

--- Aula1/04_Metodos_Parametros/Ex16_ParOuImpar.java
+++ Aula1/04_Metodos_Parametros/Ex16_ParOuImpar.java
@@ -1,8 +1,27 @@
 // ENUNCIADO:
 // Elaborar um método, em linguagem Java, com passagem de parâmetros, dentro de um programa teste, capaz de informar se o número digitado é par ou impar. A digitação do valor e a apresentação do resultado deverá acontecer externamente a este método;
 
+import java.util.Scanner;
+
 public class Ex16_ParOuImpar {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int number = scanner.nextInt();
         
+        boolean result = EvenNumber(number);
+
+        if (result) {
+            System.out.print("O numero " + number + " e PAR");
+        } else {
+            System.out.print("O numero " + number + " e IMPAR");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean EvenNumber(int n) {
+        return n % 2 == 0;
     }
 }
--- Aula1/04_Metodos_Parametros/Ex18_NumeroPrimo_Param.java
+++ Aula1/04_Metodos_Parametros/Ex18_NumeroPrimo_Param.java
@@ -6,3 +6,34 @@ public class Ex18_NumeroPrimo_Param {
         
     }
 }
+
+
+// import java.util.Scanner;
+
+// public class Ex12_NumeroPrimo {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("Digite um numero:  ");
+//         int number = scanner.nextInt();
+
+//         if(PrimeNumber(number)){
+//             System.out.print("O numero e primo.");
+//         } else{
+//             System.out.print("Numero nao e primo.");
+//         }
+//         scanner.close();
+//     }
+
+//     public static boolean PrimeNumber(int number) {
+//         if (number <= 1){
+//             return false;
+//         }
+//         for (int i = 2; i < number; i++){
+//             if (number % i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
