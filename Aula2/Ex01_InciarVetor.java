f5a3312f1f5783dc033201e7c64f34c065b0b213
Amanda Bialer <amandabialer@gmail.com>
2026-02-23 16:37:55 -0300

    ex01

--- Aula2/Ex01_InciarVetor.java
+++ Aula2/Ex01_InciarVetor.java
@@ -1,9 +1,14 @@
-package Aula2;
+// Elaborar um programa, em linguagem Java, capaz inicializar com 0 (zero) um vetor do tipo int de N elementos, utilizando laços, onde N deve ser: 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000 e 500000
 
-//  Elaborar um programa, em linguagem Java, capaz inicializar com 0 (zero) um vetor do tipo int de N elementos, utilizando laços, onde N deve ser: 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000 e 500000
-
-public class Ex01_InciarVetor {
+public class Ex01_InciarVetor{
     public static void main(String[] args) {
         
+        int[] vector = new int[500];
+        
+        for (int i = 0; i < vector.length; i++) {
+        vector[i] = 0;
+        }
+        System.out.println("Vetor de 500 elementos inicializado.");
+
     }
 }
