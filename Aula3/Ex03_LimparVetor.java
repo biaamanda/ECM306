16eaa6b5b11d8dd6314b899c67ebb850b4dc2e53
Amanda Bialer <amandabialer@gmail.com>
2026-02-26 09:33:46 -0300

    add P3

---
+++ Aula3/Ex03_LimparVetor.java
@@ -0,0 +1,7 @@
+//Elaborar um programa, em linguagem Java, capaz de limpar (colocar -1 em todas as posições) matrizes de dimensões N por M, do tipo double, onde N e M devem ser, respectivamente: 10 e 10; 50 e 75; 100 e 300; 500 e 200; 1000 e 1000; 5000 e 7000; 10000 e 1; 50000 e 25000; 100000 e 100000; e 500000 e 1000
+
+public class Ex03_LimparVetor {
+    public static void main(String[] args){
+
+    }
+}
