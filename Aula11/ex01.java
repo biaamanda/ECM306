05a407dd0ddf3ce755577ae884e40082550b6570
Amanda Bialer <amandabialer@gmail.com>
2026-05-14 09:09:28 -0300

    add aula 11

---
+++ Aula11/ex01.java
@@ -0,0 +1,25 @@
+package Aula11;
+
+/*1. Considere uma árvore que armazena valores inteiros em um nó
+com a estrutura de nós definida em aula.
+a. Escrever um método chamado imprimeFilhos() que imprime
+os valores inteiros armazenados nos filhos de um determinado nó;
+b. Escrever um método chamado pai() que é aplicado a um nó e
+retorna a referência do nó que corresponde ao seu pai;
+c. Escrever um método chamado imprimePai() que imprime o
+elemento inteiro armazenado no pai de um determinado nó;
+d. Escrever um método chamado ehInterno() que retorna um
+valor booleano true se o nó foi interno, ou false se o nó for externo;
+e. Escrever um método chamado imprimeFilhosFolhas() que
+imprime os valores inteiros armazenados nos filhos de um
+determinado nó e que também são folhas;
+f. Escrever um método chamado preorder() que quando
+aplicado à um determinado nó da árvore imprime o percurso
+preorder a partir do nó considerado;
+g. Escrever um método chamado posorder() que quando
+aplicado à um determinado nó da árvore imprime o percurso
+posorder a partir do nó considerado. */
+
+public class ex01 {
+    
+}
---
+++ Aula11/ex02.java
@@ -0,0 +1,16 @@
+package Aula11;
+
+/*
+2. Dada a árvore abaixo esquematizada:
+a. Executar o método que imprime os filhos do nó 5;
+b. Executar o método que imprime o valor armazenado no pai do nó 8;
+c. Escrever o método chamado dobraFilhos() que multiplica
+por 2 todos os nós de um determinado filho e executar essa
+função para o nó 8;
+d. Escrever um método chamado dobraPai() que multiplica por 2
+o valor armazenado no pai de um determinado nó e executar
+essa função para o nó 2 */
+
+public class ex02 {
+    
+}
